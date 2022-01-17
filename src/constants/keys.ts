98dccba213538eedcd5be0550a074179c42b0ec7
Matthew_McCracken <mccrackenmatthew73@yahoo.ca>
2022-01-17 11:44:50 -0500

    Initial dist subtree commit

---
+++ src/constants/keys.ts
@@ -0,0 +1,11 @@
+export const NasaApiKey = '74QanzWhqdF1R643za8Py5tZzcmlpRbyhtCa0o6l';
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBUy3WEdpe-eSeNsiEivO2qAliAuaQDaFU',
+    authDomain: 'kerbal-view.firebaseapp.com',
+    databaseURL: 'https://kerbal-view-default-rtdb.firebaseio.com',
+    projectId: 'kerbal-view',
+    storageBucket: 'kerbal-view.appspot.com',
+    messagingSenderId: '496669302569',
+    appId: '1:496669302569:web:29d7e4b25bdd719cba6138',
+};
