4f61b38252e6a00978bbb010b8060c1f142da7e4
Matthew_McCracken <mccrackenmatthew73@yahoo.ca>
2022-01-17 16:09:53 -0500

    sizing refactor

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 app.use(express.static('images'));
-app.get('/images/:name', cors(), (req, res) => {
-    res.sendFile(__dirname + '/images/' + req.params.name);
-});
 
 const server = app.listen(port, () => {
     console.log('server is running on port ' + port);
