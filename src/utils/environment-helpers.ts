36c48495f926d3b77e3b40021d8cce9327896542
Matthew_McCracken <mccrackenmatthew73@yahoo.ca>
2022-01-17 10:30:01 -0500

    deployment test

--- src/utils/environment-helpers.ts
+++
@@ -1,8 +0,0 @@
-const isProduction = process.env.NODE_ENV === 'production';
-const isDevelopment = process.env.NODE_ENV === 'development';
-
-export const getBaseUrl = () => {
-    return isDevelopment
-        ? 'http://localhost:3000'
-        : 'https://matthewwmcc.github.io/Kerbal-View/';
-};
